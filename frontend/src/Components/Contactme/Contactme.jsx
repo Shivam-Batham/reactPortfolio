23680db958d11e2def1d7cc0c7ddded392a0bc6a
shivam-batham <rsb7214@gmail.com>
2024-03-27 22:59:42 +0530

    contact info added

--- frontend/src/Components/Contactme/Contactme.jsx
+++ frontend/src/Components/Contactme/Contactme.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 function Contactme() {
   return (
-    <div>
-      <h1>Contact me</h1>
+    <div className='text-white p-4 '>
+      <h1>Contact me.</h1>
+      <h1>+91 6390590908</h1>
+      <h1>shivambatham2610@gmail.com</h1>
+      <hr className/>
+      comming soon. . .
     </div>
   )
 }
