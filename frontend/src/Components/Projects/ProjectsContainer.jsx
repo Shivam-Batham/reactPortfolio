311425250569aaa2653bc2059234d03aa33699bf
shivam-batham <shivambatha2610@gmail.com>
2025-02-01 19:59:08 +0530

    location master project added

--- frontend/src/Components/Projects/ProjectsContainer.jsx
+++ frontend/src/Components/Projects/ProjectsContainer.jsx
@@ -5,6 +5,7 @@ import { DarkmodeContext } from "../context/Darkmode";
 import { Link } from "react-router-dom";
 import quiz from "./quiz.png"
 import todo from "./todo.png"
+import locationmaster from "./locationmaster.png"
 function ProjectsContainer() {
   const [teckStack,setTeckState] = useState(["React", "Express","MongoDB","Node.Js"])
   const {darkmode,ToggleDarkMode} = useContext(DarkmodeContext)
@@ -21,6 +22,70 @@ function ProjectsContainer() {
           </span> */}
         </h1>
         <hr />
+        <div
+          className={`card mt-2  rounded-md sm:w-[95%] w-[95%] m-auto p-2 `}
+
+        >
+          <div className="flex place-content-between">
+            <h2 className='uppercase font-semibold'>Location Master</h2>
+            <h2>
+            <Link className="btn p-2 border mx-auto my-4 rounded-md px-4" to="https://locationsmaster.vercel.app/">
+
+              Visit
+            </Link>
+              {/* <span className="w-2 h-2 bg-red-400  rounded-full inline-block m-1">
+                {" "}
+              </span> */}
+            </h2>
+          </div>
+
+          <div className="mt-4 grid gap-2 grid-cols-6">
+            <div className="sm:col-span-2 col-span-6">
+              <img src={locationmaster} className="temp rounded object-fill w-[100%] h-[12rem]" />{" "}
+            </div>
+            <ul className="sm:col-span-4 col-span-6">
+              <li>
+                <p className="">
+                  <span className="w-1 h-1 bg-blue-400  rounded-full inline-block m-1"></span>
+                  Location Master is a Full-stack application built in Next.js & Express that provides a list of your visited location.
+                </p>{" "}
+              </li>
+              <li>
+                <p>
+                  <span className="w-1 h-1 bg-red-400   rounded-full inline-block m-1"></span>
+                  It hepls you remembering the location you have visited in the past
+                </p>{" "}
+              </li>
+               
+              <li>
+                <p>
+                  <span className="w-1 h-1 bg-yellow-400   rounded-full inline-block m-1"></span>
+                 Also showing some popuplar location for future visit.
+                </p>{" "}
+              </li>
+              <li className="flex">
+                <p>
+                <span className="w-1 h-1 bg-green-400  rounded-full inline-block m-1">
+                  {" "}
+                </span>
+                <span className="w-1 h-1 text-violet-500  inline m-1">
+                 
+                Tech Stack - 
+                </span>
+                <ul className="flex flex-wrap gap-x-4 font-bold" >
+                  {/* {teckStack.map((i)=><li className="uppercase inline m-2">{i}</li>)} */}
+                  <li className="d">Next.js</li>
+                  <li className="o">Express</li>
+                  <li className="s">MongoDB</li>
+                  <li className="b"></li>
+                </ul>
+                </p>
+              </li>
+            </ul>
+          </div>
+          
+        </div>
+        <hr className={ darkmode? " rounded w-[98%] m-auto dark-theme border-blue-500 mt-2":"rounded w-[98%] m-auto mt-2 light-theme border-blue-500 "} />
 
         <div
           className={`card mt-2  rounded-md sm:w-[95%] w-[95%] m-auto p-2 `}
